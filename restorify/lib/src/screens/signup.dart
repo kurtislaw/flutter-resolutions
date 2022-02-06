ea8a42062bad7d6ec12b91fb7b1a5d80706777cb
ShaanPurewal <89044277+ShaanPurewal@users.noreply.github.com>
2022-02-06 03:12:54 -0500

    Implement the signup page

---
+++ restorify/lib/src/screens/signup.dart
@@ -0,0 +1,137 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:restorify/src/screens/home.dart';
+
+class SignupScreen extends StatefulWidget {
+  SignupScreen({Key? key}) : super(key: key);
+
+  @override
+  _SignupScreenState createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  String _email = '';
+  String _password = '';
+  final auth = FirebaseAuth.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    double h = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+        body: Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage(
+              "https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/12/Gradient_builder_2.jpg?auto=format&q=60&w=1815&h=1020.9375&fit=crop&crop=faces"),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Column(
+        children: [
+          Container(
+            alignment: Alignment.topLeft,
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(8.0, h * 0.1, 8.0, 8.0),
+              child: FlatButton(
+                onPressed: () {
+                  Navigator.of(context).pop(context);
+                },
+                child: Text(
+                  'Back',
+                  style: TextStyle(fontSize: 30),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(8.0, h * 0.05, 175.0, 8.0),
+            child: Container(
+              width: 200,
+              height: 40,
+              alignment: Alignment.center,
+              child: TextField(
+                textAlign: TextAlign.start,
+                autocorrect: false,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                ),
+                onChanged: (value) {
+                  setState(() {
+                    _email = value.trim();
+                  });
+                },
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(8.0, 8.0, 175.0, 8.0),
+            child: Container(
+              width: 200,
+              height: 40,
+              child: TextField(
+                autocorrect: false,
+                obscureText: true,
+                decoration: InputDecoration(hintText: 'Password'),
+                onChanged: (value) {
+                  setState(() {
+                    _password = value.trim();
+                  });
+                },
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(8.0, h * 0.4, 8.0, 0.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  height: 50.0,
+                  margin: EdgeInsets.all(10),
+                  child: RaisedButton(
+                    onPressed: () {
+                      auth.createUserWithEmailAndPassword(
+                          email: _email, password: _password);
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => HomeScreen()));
+                    },
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(80.0)),
+                    padding: EdgeInsets.all(0.0),
+                    child: Ink(
+                      decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [
+                              Color.fromARGB(255, 255, 255, 255),
+                              Color.fromARGB(255, 255, 255, 255)
+                            ],
+                            begin: Alignment.centerLeft,
+                            end: Alignment.centerRight,
+                          ),
+                          borderRadius: BorderRadius.circular(30.0)),
+                      child: Container(
+                        constraints:
+                            BoxConstraints(maxWidth: 100.0, minHeight: 30.0),
+                        alignment: Alignment.center,
+                        child: Text(
+                          "Signup",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 0, 0, 0),
+                              fontSize: 15),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    ));
+  }
+}
