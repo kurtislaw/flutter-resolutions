f99d88d018a43e8149204196dcf3d4f51edb4a8b
ShaanPurewal <89044277+ShaanPurewal@users.noreply.github.com>
2022-02-09 18:02:43 -0500

    Fix Keyboard Overflow

--- restorify/lib/src/screens/login.dart
+++ restorify/lib/src/screens/login.dart
@@ -23,148 +23,149 @@ class _LoginScreenState extends State<LoginScreen> {
     double w = MediaQuery.of(context).size.width;
 
     return Scaffold(
+        resizeToAvoidBottomInset: false,
         body: Container(
-      width: double.infinity,
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: NetworkImage(
-              "https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/12/Gradient_builder_2.jpg?auto=format&q=60&w=1815&h=1020.9375&fit=crop&crop=faces"),
-          fit: BoxFit.cover,
-        ),
-      ),
-      child: Column(
-        children: [
-          Container(
-            alignment: Alignment.topLeft,
-            child: Padding(
-              padding: EdgeInsets.fromLTRB(8.0, h * 0.1, 8.0, 8.0),
-              child: FlatButton(
-                onPressed: () {
-                  Navigator.of(context).pop(context);
-                },
-                child: Text(
-                  'Back',
-                  style: TextStyle(fontSize: 30),
-                ),
-              ),
+          width: double.infinity,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage(
+                  "https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/12/Gradient_builder_2.jpg?auto=format&q=60&w=1815&h=1020.9375&fit=crop&crop=faces"),
+              fit: BoxFit.cover,
             ),
           ),
-          Padding(
-            padding: EdgeInsets.fromLTRB(8.0, h * 0.05, 175.0, 8.0),
-            child: Container(
-              width: 200,
-              height: 40,
-              alignment: Alignment.center,
-              child: TextField(
-                textAlign: TextAlign.start,
-                autocorrect: false,
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  hintText: 'Email',
-                ),
-                onChanged: (value) {
-                  setState(() {
-                    _email = value.trim();
-                  });
-                },
-              ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.fromLTRB(8.0, 8.0, 175.0, 8.0),
-            child: Container(
-              width: 200,
-              height: 40,
-              child: TextField(
-                autocorrect: false,
-                obscureText: true,
-                decoration: InputDecoration(hintText: 'Password'),
-                onChanged: (value) {
-                  setState(() {
-                    _password = value.trim();
-                  });
-                },
-              ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.fromLTRB(8.0, 10.0, 0.6 * w, 0.0),
-            child: Container(
-              height: 20,
-              width: 250,
-              child: FlatButton(
-                child: Text("Reset Password"),
-                textColor: Color.fromARGB(255, 202, 76, 67),
-                onPressed: () async {
-                  try {
-                    var test = await FirebaseAuth.instance
-                        .sendPasswordResetEmail(email: _email);
-                  } on FirebaseAuthException catch (e) {
-                    print("error resetting password");
-                  }
-                },
-              ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.fromLTRB(8.0, h * 0.4, 8.0, 0.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Container(
-                  height: 50.0,
-                  margin: EdgeInsets.all(10),
-                  child: RaisedButton(
-                    onPressed: () async {
-                      try {
-                        UserCredential userCredential = await FirebaseAuth
-                            .instance
-                            .signInWithEmailAndPassword(
-                                email: _email, password: _password);
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => DashboardScreen()));
-                      } on FirebaseAuthException catch (e) {
-                        if (e.code == 'user-not-found') {
-                          print('No user found for that email.');
-                        } else if (e.code == 'wrong-password') {
-                          print('Wrong password provided for that user.');
-                        }
-                      }
+          child: Column(
+            children: [
+              Container(
+                alignment: Alignment.topLeft,
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(8.0, h * 0.1, 8.0, 8.0),
+                  child: FlatButton(
+                    onPressed: () {
+                      Navigator.of(context).pop(context);
                     },
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(80.0)),
-                    padding: EdgeInsets.all(0.0),
-                    child: Ink(
-                      decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                            colors: [
-                              Color.fromARGB(255, 255, 255, 255),
-                              Color.fromARGB(255, 255, 255, 255)
-                            ],
-                            begin: Alignment.centerLeft,
-                            end: Alignment.centerRight,
-                          ),
-                          borderRadius: BorderRadius.circular(30.0)),
-                      child: Container(
-                        constraints:
-                            BoxConstraints(maxWidth: 100.0, minHeight: 30.0),
-                        alignment: Alignment.center,
-                        child: Text(
-                          "Login",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              color: Color.fromARGB(255, 0, 0, 0),
-                              fontSize: 15),
-                        ),
-                      ),
+                    child: Text(
+                      'Back',
+                      style: TextStyle(fontSize: 30),
                     ),
                   ),
                 ),
-              ],
-            ),
-          )
-        ],
-      ),
-    ));
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(8.0, h * 0.05, 175.0, 8.0),
+                child: Container(
+                  width: 200,
+                  height: 40,
+                  alignment: Alignment.center,
+                  child: TextField(
+                    textAlign: TextAlign.start,
+                    autocorrect: false,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      hintText: 'Email',
+                    ),
+                    onChanged: (value) {
+                      setState(() {
+                        _email = value.trim();
+                      });
+                    },
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(8.0, 8.0, 175.0, 8.0),
+                child: Container(
+                  width: 200,
+                  height: 40,
+                  child: TextField(
+                    autocorrect: false,
+                    obscureText: true,
+                    decoration: InputDecoration(hintText: 'Password'),
+                    onChanged: (value) {
+                      setState(() {
+                        _password = value.trim();
+                      });
+                    },
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(8.0, 10.0, 0.6 * w, 0.0),
+                child: Container(
+                  height: 20,
+                  width: 250,
+                  child: FlatButton(
+                    child: Text("Reset Password"),
+                    textColor: Color.fromARGB(255, 202, 76, 67),
+                    onPressed: () async {
+                      try {
+                        var test = await FirebaseAuth.instance
+                            .sendPasswordResetEmail(email: _email);
+                      } on FirebaseAuthException catch (e) {
+                        print("error resetting password");
+                      }
+                    },
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(8.0, h * 0.4, 8.0, 0.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(
+                      height: 50.0,
+                      margin: EdgeInsets.all(10),
+                      child: RaisedButton(
+                        onPressed: () async {
+                          try {
+                            UserCredential userCredential = await FirebaseAuth
+                                .instance
+                                .signInWithEmailAndPassword(
+                                    email: _email, password: _password);
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) => DashboardScreen()));
+                          } on FirebaseAuthException catch (e) {
+                            if (e.code == 'user-not-found') {
+                              print('No user found for that email.');
+                            } else if (e.code == 'wrong-password') {
+                              print('Wrong password provided for that user.');
+                            }
+                          }
+                        },
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(80.0)),
+                        padding: EdgeInsets.all(0.0),
+                        child: Ink(
+                          decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                colors: [
+                                  Color.fromARGB(255, 255, 255, 255),
+                                  Color.fromARGB(255, 255, 255, 255)
+                                ],
+                                begin: Alignment.centerLeft,
+                                end: Alignment.centerRight,
+                              ),
+                              borderRadius: BorderRadius.circular(30.0)),
+                          child: Container(
+                            constraints: BoxConstraints(
+                                maxWidth: 100.0, minHeight: 30.0),
+                            alignment: Alignment.center,
+                            child: Text(
+                              "Login",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  color: Color.fromARGB(255, 0, 0, 0),
+                                  fontSize: 15),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
